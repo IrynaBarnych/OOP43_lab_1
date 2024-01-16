a80669d3ef143f5427ac57ca4d7fe1673600941d
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-16 23:04:38 +0200

    Вивести прізвища лікарів, зарплата (сума ставки та надбавки 120) яких перевищує 1500.

--- OOP43_lab.sql
+++ OOP43_lab.sql
@@ -1,5 +1,4 @@
--- 9. Вивести назви, корпуси та фонди фінансування відділень, які знаходяться у корпусах3 або 6 та мають
---фонд фінансування менший, ніж 11000 або більший за 25000.
-SELECT Name, Building, Financing FROM Departments
-WHERE (Building = 3 OR Building = 6) 
-      AND (Financing < 11000 OR Financing > 25000);
+-- 10. Вивести прізвища лікарів, зарплата (сума ставки та
+-- надбавки 120) яких перевищує 1500.
+SELECT Surname, Salary FROM Doctors
+WHERE (Salary + 120) > 1500;
