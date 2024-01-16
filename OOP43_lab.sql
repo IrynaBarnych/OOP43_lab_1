2759d2aa2091e5ddd891bf95d0898bc01da0f567
IrynaBarnych <iryna.barnych@gmail.com>
2024-01-16 23:14:04 +0200

    Вивести назви захворювань усіх ступенів тяжкості, крім 1-го та 2-го.

--- OOP43_lab.sql
+++ OOP43_lab.sql
@@ -1,7 +1,8 @@
--- 13. Вивести назви та номери корпусів відділень, 
--- які знаходяться у корпусах 1, 3, 8 або 10.
+-- 14. Вивести назви захворювань усіх ступенів тяжкості,
+-- крім 1-го та 2-го.
 
-SELECT Name, Building FROM Departments
-WHERE Building IN (1, 3, 8, 10);
+SELECT Name FROM Diseases
+WHERE Severity NOT IN (1, 2);
+
 
 
